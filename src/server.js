2fdc429501c9c58c69382bf5cad396d15951baa0
Ian Andwati <andwatiian@gmail.com>
2024-04-29 22:24:12 +0300

    prettier format

--- src/server.js
+++ src/server.js
@@ -1,36 +1,32 @@
-const express = require('express');
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
-const path = require('path');
-
+const express = require("express");
+const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+const path = require("path");
 
 const app = express();
 
 dotenv.config();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Route to handle form submission
-app.post('/submit-data', async (req, res) => {
-
+app.post("/submit-data", async (req, res) => {
   console.log(req.body);
   const name = req.body.name;
   const email = req.body.email;
-
 
   try {
     const connection = await pool.getConnection();
@@ -38,13 +34,13 @@ app.post('/submit-data', async (req, res) => {
     const [results] = await connection.execute(sql, [name, email]);
 
     connection.release();
-    res.send('Data saved successfully!');
+    res.send("Data saved successfully!");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error saving data!');
+    res.status(500).send("Error saving data!");
   }
 });
 
 app.listen(port, () => {
-  console.log(`Go to http://localhost:3000 in your browser` )
+  console.log(`Go to http://localhost:3000 in your browser`);
 });
